6e5a94f51229d3753caf95597d6b27b25fd9f16c
Marco <m_uno_p@hotmail.com>
2024-09-23 19:03:15 -0500

    Add workflow for building, publishing, and deploying microservice. Use of GitHub secrets

--- chapter_08/example-3/scripts/build-image.ps1
+++ chapter_08/example-3/scripts/build-image.ps1
@@ -1,1 +1,1 @@
-docker build -t $Env:CONTAINER_REGISTRY_URL/video-streaming:$Env:VERSION -f Dockerfile.prod .
+docker build -t $Env:CONTAINER_REGISTRY_URL/video-streaming:$Env:VERSION -f chapter_08/example-3/Dockerfile.prod chapter_08/example-3/
